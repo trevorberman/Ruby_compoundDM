bca0e1b5e149c124788bb47450c3647e588d45d3
trevorberman <trevorberman@users.noreply.github.com>
2014-12-05 20:15:12 -0600

    fixed pagination element and calculation

--- scrape.rb
+++ scrape.rb
@@ -12,11 +12,8 @@ base_uri = 'http://collections.lib.uwm.edu/cdm/search/collection/mkenh/searchter
 doc = Nokogiri::HTML(open(base_uri))
 
 # Find the number of additional results pages
-pagination = doc.css('div.link_bar_pagination li')
-# Because <li></li>s each
-# Divide by 2 changes .length from num elements to num buttons
-# Subtract 2 eliminates '1st'(active-page) and 'Next' buttons
-more_results_pages = (pagination.length / 2) - 2
+pagination = doc.css('li#pagination_button_last a')
+more_results_pages = pagination[0].text.to_i - 1
 puts more_results_pages
 
 # Create 'General Delimited Syntax' array
@@ -30,28 +27,26 @@ end
 # Can use var to set array length!
 # But .inset into empty array is simpler solution here.
 # pages = Array.new(more_results_pages)
+
+# Create array to hold more_result_pages uri list
 pages = []
 
+# Ouput tests on the new array
 puts pages.length
 p pages
 
+# Add page/# ending for each results_page to base uri and add to uri list
 while  more_results_pages > 0
   more_results_uri = base_uri + "/page/#{more_results_pages + 1}"
   # Can use var and arithm to index array!
   # But .inset into empty array is simpler solution here.
   # pages.insert(more_results_pages.to_i - 1, more_results_uri)
   pages.insert(0, more_results_uri)
-  # puts more_results_uri
-  # pages.each { |uri| puts uri }
   p pages
-  # puts pages.first
-  # puts pages.last
   more_results_pages -= 1
   puts more_results_pages
 end
 
+# Append pages[] to uri_list[]
 pages.each { |uri| uri_list << uri }
 p uri_list
-
-# !!!??? why Eastside query only giving 3 additional pages
-# when should be 4; from buttons 2, 3, 4, 5 ???!!!
